02fe94d81459fff035718f5922d84c29b8d5b590
Vladyslav Slobodianiuk <vs150502@gmail.com>
2021-09-08 16:16:31 +0300

    Add change datadog url

--- modules/booksapi_cluster/main.tf
+++ modules/booksapi_cluster/main.tf
@@ -16,49 +16,46 @@ resource "aws_ecs_task_definition" "service" {
 
   container_definitions = jsonencode([
     {
-      name = "first"
-      image = "vsua/booksapi:latest"
-      cpu = 128
-      memory = 512
+      name      = "first"
+      image     = "vsua/booksapi:latest"
+      cpu       = 128
+      memory    = 512
       essential = true
       environment = [
-        {
-          name = "ACCESS_KEY",
-          value = var.access_key
+        {name = "ACCESS_KEY", value = var.access_key
         },
-        {
-          name = "SECRET_KEY",
-          value = var.secret_key
+        {name = "SECRET_KEY", value = var.secret_key
         },
-        {
-          name = "REGION_NAME",
-          value = var.region_name
+        {name = "REGION_NAME", value = var.region_name
         }
       ]
       portMappings = [
         {
           containerPort = 5000
-          hostPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    },
+    {
+      name = "datadog-agent"
+      image = "datadog/agent:latest"
+      cpu       = 128
+      memory    = 512
+      environment = [
+        {
+          name = "DD_API_KEY",
+          value = var.dd_api_key
+        },
+        {
+          name = "ECS_FARGATE",
+          value = "true"
+        },
+        {
+          name = "DD_SITE",
+          value = "datadoghq.eu"
         }
       ]
     }
-//    },
-//    {
-//      name = "datadog-agent"
-//      image = "datadog/agent:latest"
-//      cpu       = 128
-//      memory    = 512
-//      environment = [
-//        {
-//          name = "DD_API_KEY",
-//          value = var.dd_api_key
-//        },
-//        {
-//          name = "ECS_FARGATE",
-//          value = "true"
-//        }
-//      ]
-//    }
     ]
   )
 }
